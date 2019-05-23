98ac9dd5f40e117479c5ab2e60918ffb275c19c6
António Ribeiro da Costa <amrpcosta@gmail.com>
2019-05-23 16:55:35 +0100

    stats script

--- stats/script-depth5.sh
+++ stats/script-depth5.sh
@@ -8,12 +8,12 @@ depth=5
 
 for i in {2..6}
     do for j in {0..9}
-            do java -cp ../out/production/NineMensMorris/ NineMensMorris $depth 1 $i 1 > data/d$depth-A1A$i-[$j]
-        done
+        do java -cp ../out/production/NineMensMorris/ NineMensMorris $depth 1 $i 1 > data/d$depth-A1A"$i"-[$j]
+    done
 done
 
 for i in {2..6}
     do for j in {0..9}
-            do java -cp ../out/production/NineMensMorris/ NineMensMorris $depth $i 1 1 > data/d$depth-A$iA1-[$j]
-        done
+        do java -cp ../out/production/NineMensMorris/ NineMensMorris $depth $i 1 1 > data/d$depth-A"$i"A1-[$j]
+    done
 done
